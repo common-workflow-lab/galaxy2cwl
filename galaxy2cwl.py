9a46256acf79fd7f838a29fd274032ebe7cce894
Peter Amstutz <peter.amstutz@curoverse.com>
2015-07-11 08:50:03 +0100

    Use DOM directly instead of trying to be clever

--- galaxy2cwl.py
+++ galaxy2cwl.py
@@ -3,100 +3,83 @@ import argparse
 import pprint
 import os
 
-def dictify(elm):
-    d = {}
-    for i in xrange(0, elm.attributes.length):
-        attr = elm.attributes.item(i)
-        d[attr.name] = attr.value
-    for i in elm.childNodes:
-        if isinstance(i, xml.dom.minidom.Element):
-            if i.tagName not in d:
-                d[i.tagName] = []
-            if len(i.childNodes) == 1 and isinstance(i.firstChild, xml.dom.minidom.Text):
-                if i.attributes.length == 0:
-                    d[i.tagName].append(i.firstChild.data)
-                else:
-                    d[i.tagName].append(dictify(i))
-                    d[i.tagName][-1]["data"] = i.firstChild.data
-            else:
-                d[i.tagName].append(dictify(i))
-
-    return d
-
 def inpschema(elm, expands, top=False):
     schema = []
 
-    for cond in elm.get("conditional", []):
-        sch = {}
-        if top:
-            sch["id"] = "#" + cond["name"]
-        else:
-            sch["name"] = cond["name"]
-
-        sch["type"] = []
-
-        for when in cond["when"]:
-            f = {"type": "record", "name": when["value"]}
-            f["fields"] = inpschema(when, expands)
-            f["fields"].append({
-                "name": cond["param"][0]["name"],
-                "type": "enum",
-                "symbols": [when["value"]]
-                })
-            sch["type"].append(f)
-
-        schema.append(sch)
-
-    for param in elm.get("param", []):
-        sch = {}
-
-        if top:
-            sch["id"] = "#" + param["name"]
-        else:
-            sch["name"] = param["name"]
-
-        sch["label"] = param.get("label")
-
-        if param["type"] == "data":
-            sch["type"] = "File"
-        elif param["type"] == "select":
-            e = {"type": "enum"}
-            e["symbols"] = []
-            for opt in param.get("option", []):
-                e["symbols"].append(opt["value"])
-            if len(e["symbols"]) == 0:
-                sch = None
+    for e in elm.childNodes:
+        if not isinstance(e, xml.dom.minidom.Element):
+            continue
+        if e.tagName == "conditional":
+            sch = {}
+            if top:
+                sch["id"] = "#" + e.getAttribute("name")
             else:
-                sch["type"] = e
-        elif param["type"] == "text":
-            sch["type"] = "string"
-        elif param["type"] == "integer":
-            sch["type"] = "int"
-        else:
-            sch["type"] = "Any"
+                sch["name"] = e.getAttribute("name")
 
-        if sch:
-            if param.get("optional") == "True":
-                sch["type"] = ["null", sch["type"]]
-            if param.get("value"):
-                sch["default"] = param.get("value")
-                if sch["type"] == "int":
-                    sch["default"] = int(sch["default"])
+            sch["type"] = []
+
+            for when in e.getElementsByTagName("when"):
+                f = {"type": "record", "name": when.getAttribute("value")}
+                f["fields"] = inpschema(when, expands)
+                f["fields"].append({
+                    "name": e.getElementsByTagName("param")[0].getAttribute("name"),
+                    "type": "enum",
+                    "symbols": [when.getAttribute("value")]
+                    })
+                sch["type"].append(f)
 
             schema.append(sch)
 
-    for ex in elm.get("expand", []):
-        schema.extend(inpschema(expands[ex["macro"]], expands))
+        elif e.tagName == "cond":
+            sch = {}
+
+            if top:
+                sch["id"] = "#" + e.getAttribute("name")
+            else:
+                sch["name"] = e.getAttribute("name")
+
+            sch["label"] = e.getAttribute("label")
+
+            if param["type"] == "data":
+                sch["type"] = "File"
+            elif param["type"] == "select":
+                e = {"type": "enum"}
+                e["symbols"] = []
+                for opt in e.getElementsByTagName("option"):
+                    e["symbols"].append(opt.getAttribute("value"))
+                if len(e["symbols"]) == 0:
+                    sch = None
+                else:
+                    sch["type"] = e
+            elif param["type"] == "text":
+                sch["type"] = "string"
+            elif param["type"] == "integer":
+                sch["type"] = "int"
+            else:
+                sch["type"] = "Any"
+
+            if sch:
+                if param.get("optional") == "True":
+                    sch["type"] = ["null", sch["type"]]
+                if param.get("value"):
+                    sch["default"] = e.getAttribute("value")
+                    if sch["type"] == "int":
+                        sch["default"] = int(sch["default"])
+
+                schema.append(sch)
+
+        elif e.tagName == "expand":
+            schema.extend(inpschema(expands[e.getAttribute("macro")], expands))
 
     return schema
 
 def macros(basedir, elm, toks, expands):
-    for imp in elm.get("import", []):
-        macros(basedir, dictify(xml.dom.minidom.parse(os.path.join(basedir, imp)).documentElement), toks, expands)
-    for tok in elm.get("token", []):
-        toks[tok["name"]] = tok["data"]
-    for x in elm.get("xml", []):
-        expands[x["name"]] = x
+    for imp in elm.getElementsByTagName("import"):
+        macros(basedir, xml.dom.minidom.parse(os.path.join(basedir, imp.firstChild.data)).documentElement, toks, expands)
+    for tok in elm.getElementsByTagName("token"):
+        toks[tok.getAttribute("name")] = tok.firstChild.data
+    for x in elm.getElementsByTagName("xml"):
+        expands[x.getAttribute("name")] = elm
 
 def main():
     parser = argparse.ArgumentParser()
@@ -107,24 +90,21 @@ def main():
 
     dom = xml.dom.minidom.parse(args.tool)
 
-    tool = dictify(dom.documentElement)
+    tool = dom.documentElement
 
     cwl = {"class":"CommandLineTool"}
 
-    cwl["id"] = "#" + tool["id"]
-    cwl["label"] = tool["name"]
+    cwl["id"] = "#" + tool.getAttribute("id")
+    cwl["label"] = tool.getAttribute("name")
 
     cwl["baseCommand"] = ["/bin/sh", "-c"]
 
-    if "interpreter" in tool["command"][0]:
-        interpreter = tool["command"][0]["interpreter"] + " "
-    else:
-        interpreter = ""
+    interpreter = tool.getElementsByTagName("command")[0].getAttribute("interpreter")
 
     cwl["arguments"] = [{
         "valueFrom": {
             "engine": "#cheetah",
-            "script": interpreter + tool["command"][0]
+            "script": interpreter + tool.getElementsByTagName("command")[0].firstChild.data
             }
         }]
 
@@ -132,12 +112,12 @@ def main():
 
     toks = {}
     expands = {}
-    macros(basedir, tool["macros"][0], toks, expands)
+    macros(basedir, tool.getElementsByTagName("macros")[0], toks, expands)
 
     #pprint.pprint(toks)
     #pprint.pprint(expands)
 
-    cwl["inputs"].extend(inpschema(tool["inputs"][0], expands, top=True))
+    cwl["inputs"].extend(inpschema(tool.getElementsByTagName("inputs")[0], expands, top=True))
 
     pprint.pprint(cwl)
 
